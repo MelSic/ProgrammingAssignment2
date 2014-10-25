e4354349a4fa0f20fc74832ab4bf21e639f672bb
MelSic <melanie.sickinger@limango.de>
2014-10-25 16:37:22 +0200

    Assignment2
    changed template to my solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below two functions are built to create a special object 
+## that stores a matrix and caches its inverse matrix.
 
-## Write a short comment describing this function
+
+## The function makeCacheMatrix creates a list containing a function 
+## to set and get the values of a matrix and set and get the values 
+## of the inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        get <- function() {
+                x
+        }
+        setinv <- function(solve) {
+                inv <<- solve
+        }
+        getinv <- function() {
+                inv
+        }
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function checks if there has already been calculated 
+## the inverse matrix of the matrix defined in makeCacheMatrix. If so, it
+## gets the inverse from the cache and skips calculation of the inverse, 
+## otherwise it calculates it and sets the inverse in the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
